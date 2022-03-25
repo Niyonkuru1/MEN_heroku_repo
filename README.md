d9cfc5f2e6e6aa271422f1a961a47ca6a8553199
niyonkuru1 <sylvainniyonkuru2@gmail.com>
2022-03-25 13:21:44 +0200

    changing the number of badges to small number and removing the reporter in the main.yaml file

--- README.md
+++ README.md
@@ -11,8 +11,6 @@
   - Refer to the documentation page ***[here](https://my-brand-men-heroku.herokuapp.com/docs/)***.
 
 ## TEST BADGE:
-- Overals ![](https://img.shields.io/badge/Coverage-74%25-5A7302.svg?prefix=$coverage$)
-- Statements ![](https://img.shields.io/badge/Coverage-81%25-83A603.svg?style=flat&logo=kotlin&logoColor=white&color=blue&prefix=$statements$)
-- Branches ![](https://img.shields.io/badge/Coverage-59%25-F2E96B.svg?style=social&logo=ktor&logoColor=black&color=red&prefix=$branches$)
-- Functions![](https://img.shields.io/badge/Coverage-74%25-5A7302.svg?prefix=$functions$)
+- Statements: ![](https://img.shields.io/badge/Coverage-81%25-83A603.svg?style=flat&logo=kotlin&logoColor=white&color=blue&prefix=$statements$)
+- Functions: ![](https://img.shields.io/badge/Coverage-74%25-5A7302.svg?prefix=$functions$)
 - Lines![](https://img.shields.io/badge/Coverage-81%25-83A603.svg?prefix=$lines$)
--- test/message.test.js
+++ test/message.test.js
@@ -1,72 +1,72 @@
-// process.env.NODE_ENV = "test";
+process.env.NODE_ENV = "test";
 
-// import Messagedb from "../server/model/messageModel";
-// import chai from "chai";
-// import chaiHttp from "chai-http";
-// import app from "../server";
-// const expect = chai.expect;
-// chai.should();
-// chai.use(chaiHttp);
+import Messagedb from "../server/model/messageModel";
+import chai from "chai";
+import chaiHttp from "chai-http";
+import app from "../server";
+const expect = chai.expect;
+chai.should();
+chai.use(chaiHttp);
 
-// before((done) => {
-//     Messagedb.deleteMany({}, function (err) { });
-//     done();
-// })
+before((done) => {
+    Messagedb.deleteMany({}, function (err) { });
+    done();
+})
 
-// after((done) => {
-//     Messagedb.deleteMany({}, function (err) { });
-//     done();
-// })
+after((done) => {
+    Messagedb.deleteMany({}, function (err) { });
+    done();
+})
 
-// describe('/api/messages TEST on the messagees_DB Collection', () => {
-//     it("should verify that we have 0 messages in the DB", (done) => {
-//         chai.request(app)
-//             .get("/api/messages")
-//             .end((err, res) => {
-//                 let messageData = res.body;
-//                 //using expect
-//                 expect(res).to.have.status(202);
-//                 expect(messageData).to.be.a('array');
-//                 expect(messageData).to.have.lengthOf(0);
-//                 done()
-//             })
-//     })
+describe('/api/messages TEST on the messagees_DB Collection', () => {
+    it("should verify that we have 0 messages in the DB", (done) => {
+        chai.request(app)
+            .get("/api/messages")
+            .end((err, res) => {
+                let messageData = res.body;
+                //using expect
+                expect(res).to.have.status(202);
+                expect(messageData).to.be.a('array');
+                expect(messageData).to.have.lengthOf(0);
+                done()
+            })
+    })
 
-//     it("should POST a valid  messages in the database", (done) => {
-//         let titleArr = ['one', 'two', 'three'];
-//         for (let i = 0; i < titleArr.length; i++) {
-//            let message = {
-//                 name: titleArr[i] + "the game",
-//                 email: titleArr[i] + "@gmail.com",
-//                 phone: titleArr[i] + " 0784555555555",
-//                 message: " welcome to the messages database"
-//             }
-//             chai.request(app)
-//                 .post("/api/messages")
-//                 .send(message)
-//                 .end((err, res) => {
-//                     let messageData = res.body;
-//                     res.should.have.status(201);
-//                     res.body.should.be.a('object');
-//                     expect(messageData).to.have.all.keys('name', 'email', 'phone',"message", "__v", "_id", "date");
-//                     expect(messageData.name, messageData.email, messageData.phone, messageData.message).to.be.a('string');
-//                 })
-//         }
-//         done();
-//     })
+    it("should POST a valid  messages in the database", (done) => {
+        let titleArr = ['one', 'two', 'three'];
+        for (let i = 0; i < titleArr.length; i++) {
+           let message = {
+                name: titleArr[i] + "the game",
+                email: titleArr[i] + "@gmail.com",
+                phone: titleArr[i] + " 0784555555555",
+                message: " welcome to the messages database"
+            }
+            chai.request(app)
+                .post("/api/messages")
+                .send(message)
+                .end((err, res) => {
+                    let messageData = res.body;
+                    res.should.have.status(201);
+                    res.body.should.be.a('object');
+                    expect(messageData).to.have.all.keys('name', 'email', 'phone',"message", "__v", "_id", "date");
+                    expect(messageData.name, messageData.email, messageData.phone, messageData.message).to.be.a('string');
+                })
+        }
+        done();
+    })
 
-//     it("Should retrieve messages from database", (done) => {
-//         let len;
-//         chai.request(app)
-//             .get("/api/messages")
-//             .end((err, res) => {
-//                 var messages = res.body;
-//                 len = messages.length;
-//                 res.should.have.status(202);
-//                 expect(messages).to.be.an("array");
-//                 expect(messages[0]).to.be.an('object');
-//                 expect(len).to.not.be.an('undefined');
-//                 done();
-//                 })
-//             })
-//     })
+    it("Should retrieve messages from database", (done) => {
+        let len;
+        chai.request(app)
+            .get("/api/messages")
+            .end((err, res) => {
+                var messages = res.body;
+                len = messages.length;
+                res.should.have.status(202);
+                expect(messages).to.be.an("array");
+                expect(messages[0]).to.be.an('object');
+                expect(len).to.not.be.an('undefined');
+                done();
+                })
+            })
+    })
